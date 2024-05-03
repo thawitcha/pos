51ae32cd44aaa60549a670b225e3f284a1a2e474
Thawit <thawitcha.w@kkumail.com>
2024-05-03 17:16:57 +0700

    connect backend

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  getBackend:any
+  foods: any = [];
+  constructor(
+    private backEnd:HttpClient
+  ) { }
 
-  constructor() { }
-
-  ngOnInit():void {0
+  ngOnInit():void {
+    this.loadFood()
   }
+  loadFood() {
 
+    this.getBackend = this.backEnd.get('http://localhost/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
+      this.foods = data;
+    console.log(this.foods);
+    
+    })
+
+
+
+  }
 }
