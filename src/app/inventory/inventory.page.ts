536ba1420d353a28a66d3fa72097a37e81454a7e
Thawit <thawitcha.w@kkumail.com>
2024-05-08 18:10:30 +0700

    หน้าสินค้าคงคลัง และหน้าขายนิดหน่อย

--- src/app/inventory/inventory.page.ts
+++ src/app/inventory/inventory.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.page.html',
   styleUrls: ['./inventory.page.scss'],
 })
 export class InventoryPage implements OnInit {
+  getBackend:any
+  foods: any = [];
+  Daily_sales : any
+  totalPrice :number = 0
+  constructor(
+    private backEnd:HttpClient
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit():void {
+    this.loadFood()
+    this.cal()
   }
+  loadFood() {
 
+    this.getBackend = this.backEnd.get('http://192.168.1.169/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
+      this.foods = data;
+    console.log(this.foods);
+
+    })
+  }
+  cal() {
+    this.Daily_sales = this.backEnd.get('http://192.168.1.169/POSproject/index.php/bill/getOrderBill').subscribe((data: any) => {
+      // คำนวณราคารวม
+      this.totalPrice = data.map((item: any) => parseInt(item.price) * parseInt(item.total)).reduce((acc: number, curr: number) => acc + curr, 0);
+
+      console.log(this.totalPrice); // แสดงค่าราคารวมในคอนโซล
+    })
+  }
+  
 }
