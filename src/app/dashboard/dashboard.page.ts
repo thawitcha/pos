7f4142a19a95213f05aa9c37aeaa83b2d194f1f7
Pond236 <chatchai.pha@kkumail.com>
2024-05-10 17:12:10 +0700

    Super update import tool  for create chart update dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,20 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// import { ApexAxisChartSeries, ApexChart, ApexNonAxisChartSeries, ApexTitleSubtitle } from 'ng-apexcharts';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexStroke
+} from "ng-apexcharts";
+export type ChartOptions = {
+  series?: ApexAxisChartSeries;
+  chart?: ApexChart;
+  xaxis?: ApexXAxis;
+  stroke?: ApexStroke;
+  tooltip?: ApexTooltip;
+  dataLabels?: ApexDataLabels;
+};
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
-export class DashboardPage implements OnInit {
-  getBackend:any
-  foods: any = [];
-  Daily_sales : any
-  totalPrice :number = 0
-  constructor(
-    private backEnd:HttpClient
-  ) { }
 
-  ngOnInit():void {
+export class DashboardPage implements OnInit {
+  @ViewChild("chart") chart?: ChartComponent;
+   chartOptions?: Partial<ChartOptions> |any;
+
+  getBackend: any
+  foods: any = [];
+  Daily_sales: any
+  totalPrice: number = 0
+  constructor(
+    private backEnd: HttpClient
+  ) { this.chartOptions = {
+    series: [
+      {
+        name: "series1",
+        data: [31, 40, 28, 51, 42, 109, 100]
+      },
+      {
+        name: "series2",
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "area"
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: "smooth"
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "2018-09-19T00:00:00.000Z",
+        "2018-09-19T01:30:00.000Z",
+        "2018-09-19T02:30:00.000Z",
+        "2018-09-19T03:30:00.000Z",
+        "2018-09-19T04:30:00.000Z",
+        "2018-09-19T05:30:00.000Z",
+        "2018-09-19T06:30:00.000Z"
+      ]
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm"
+      }
+    }
+  };}
+
+  ngOnInit(): void {
     this.loadFood()
     this.cal()
   }
@@ -22,7 +82,7 @@ export class DashboardPage implements OnInit {
 
     this.getBackend = this.backEnd.get('http://192.168.1.169/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
       this.foods = data;
-    console.log(this.foods);
+      console.log(this.foods);
 
     })
   }
