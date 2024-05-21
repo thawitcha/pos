8ceacd4bb04c906771799a08ee998029c18600dd
Pond236 <chatchai.pha@kkumail.com>
2024-05-21 18:01:08 +0700

    scan barcode update

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -80,14 +80,14 @@ export class DashboardPage implements OnInit {
   }
   loadFood() {
 
-    this.getBackend = this.backEnd.get('http://192.168.1.169/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
+    this.getBackend = this.backEnd.get('http://192.168.1.104/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
       this.foods = data;
       console.log(this.foods);
 
     })
   }
   cal() {
-    this.Daily_sales = this.backEnd.get('http://192.168.1.169/POSproject/index.php/bill/getOrderBill').subscribe((data: any) => {
+    this.Daily_sales = this.backEnd.get('http://192.168.1.104/POSproject/index.php/bill/getOrderBill').subscribe((data: any) => {
       // คำนวณราคารวม
       this.totalPrice = data.map((item: any) => parseInt(item.price) * parseInt(item.total)).reduce((acc: number, curr: number) => acc + curr, 0);
 
--- src/app/sale/sale.page.ts
+++ src/app/sale/sale.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-sale',
@@ -14,8 +15,10 @@ export class SalePage implements OnInit {
   totalPrice: number = 0
   type: number = 1
   foods_search: any = [];
+  timeout: any
+  in_cart: any = [];
 
-
+  @ViewChild('input') input!: IonInput;
   constructor(
     private backEnd: HttpClient
   ) { }
@@ -25,37 +28,48 @@ export class SalePage implements OnInit {
     this.cal()
 
   }
+  ionViewDidEnter() {
+    this.input.setFocus();
+  }
   loadFood() {
 
-    this.getBackend = this.backEnd.get('http://192.168.1.169/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
+    this.getBackend = this.backEnd.get('http://192.168.1.104/POSproject/index.php/foodGroup/getMenuAll').subscribe((data: any) => {
       this.foods = data;
       console.log(this.foods);
 
     })
   }
   cal() {
-    this.Daily_sales = this.backEnd.get('http://192.168.1.169/POSproject/index.php/bill/getOrderBill').subscribe((data: any) => {
+    this.Daily_sales = this.backEnd.get('http://192.168.1.104/POSproject/index.php/bill/getOrderBill').subscribe((data: any) => {
       // คำนวณราคารวม
       this.totalPrice = data.map((item: any) => parseInt(item.price) * parseInt(item.total)).reduce((acc: number, curr: number) => acc + curr, 0);
 
       console.log(this.totalPrice); // แสดงค่าราคารวมในคอนโซล
     })
   }
-  timeout:any
+
   handleInput(event: any) {
     const query = event.target.value.toLowerCase();
     const formData = new FormData();
     if (this.timeout) {
-        clearTimeout(this.timeout);
+      clearTimeout(this.timeout);
     }
     this.timeout = setTimeout(() => {
-        formData.append('search', query);
+      formData.append('search', query);
 
-        this.backEnd.post('http://192.168.1.169/POSproject/index.php/foodGroup/getMenuSearch', formData).subscribe((data: any) => {
-            this.foods_search = data;
-            console.log(this.foods_search);
-        });
-    }, 2000);
-}
+      this.backEnd.post('http://192.168.1.104/POSproject/index.php/foodGroup/getMenuSearch', formData).subscribe((data: any) => {
+        this.foods_search = data;
+        if (data && data.length > 0) {
+          this.in_cart.push(data[0])
+        }
+
+        console.log(this.foods_search);
+        console.log("cart", this.in_cart);
+
+
+      });
+    }, 500);
+  }
+
 
 }
