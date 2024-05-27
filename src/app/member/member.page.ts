aa1fbf03540b718a0684cde39c9ce9d3838ef128
Thawit <thawitcha.w@kkumail.com>
2024-05-27 18:04:14 +0700

    member

--- src/app/member/member.page.ts
+++ src/app/member/member.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-member',
   templateUrl: './member.page.html',
   styleUrls: ['./member.page.scss'],
 })
 export class MemberPage implements OnInit {
+  getBackend: any
+  member: any = [];
+  constructor( private backEnd: HttpClient ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit():void {
+    this.loadMember()
   }
+  loadMember() {
 
+    this.getBackend = this.backEnd.get('http://192.168.1.104/POSproject/index.php/Member/getMembersBySearch').subscribe((data: any) => {
+      this.member = data;
+      console.log(this.member);
+
+    })
+  }
 }
